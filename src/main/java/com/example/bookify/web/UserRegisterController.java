07cf9bcdbbcc2d9e7553f4dffc35d5862b89fd83
Kalin <hattomme@abv.bg>
2022-07-08 11:14:21 +0300

    User Login and Register functionality added.

--- src/main/java/com/example/bookify/web/UserRegisterController.java
+++ src/main/java/com/example/bookify/web/UserRegisterController.java
@@ -1,24 +1,37 @@
 package com.example.bookify.web;
 
 import com.example.bookify.model.dto.UserRegisterDTO;
+import com.example.bookify.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserRegisterController {
 
+    private final UserService userService;
+
+    public UserRegisterController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/users/register")
     public String register() {
 
-        return "register";
+        return "register-auth";
     }
 
     @PostMapping("/users/register")
     public String registerConfirm(UserRegisterDTO userRegisterDTO) {
 
+        userService.registerAndLogin(userRegisterDTO);
 
+        return "redirect:login";
+    }
 
-        return "redirect:/login";
+    @ModelAttribute
+    public UserRegisterDTO userRegisterDTO() {
+        return new UserRegisterDTO();
     }
 }
